3041e7fd6f10cea69d1eaa1775cba0dfa9954486
Pedro <pedro.argentatii@gmail.com>
2026-02-19 11:07:45 -0300

    Initial Commit

---
+++ src/main/java/br/com/fiap/hackathon_notification/HackathonNotificationApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.hackathon_notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/hackathon_notification/HackathonNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.hackathon_notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
